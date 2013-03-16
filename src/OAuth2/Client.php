a96d11d89b5d6d0610e852e13b7cbccaa522168c
Chris Harvey <chris@chrisnharvey.com>
2013-03-16 12:29:49 +0000

    Overloading getBaseUrl method from Guzzle HTTP Client

--- src/OAuth2/Client.php
+++ src/OAuth2/Client.php
@@ -24,15 +24,24 @@ class Client extends GuzzleClient
         return $this;
     }
 
-    public function setBaseUrl($url)
+    public function getBaseUrl($expand = true)
     {
-        $url .= strpos($url, '?') ? '&' : '?';
-        $url .= http_build_query(array(
-            'access_token' => $this->tokens->access_token
-        ));
+        $baseUrl = parent::getBaseUrl(false);
+        $url = $baseUrl;
 
-        $this->baseUrl = $url;
+        if ($this->tokens) {
+            $url .= strpos($url, '?') ? '&' : '?';
+            $url .= http_build_query(array(
+                'access_token' => $this->tokens->access_token
+            ));
+        }
 
-        return $this;
+        $this->setBaseUrl($url);
+
+        $return = parent::getBaseUrl($expand);
+
+        $this->setBaseUrl($baseUrl);
+
+        return $return;
     }
 }
